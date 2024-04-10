874f6a934f35c22b908b99e090df2a544edde1dd
Vyacheslav Kan <vkan745@gmail.com>
2024-04-10 09:29:12 +0500

    3. Каркас шахматной доски [#792] (добавлен перебор массива figures в методе free)

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -2,6 +2,7 @@ package ru.job4j.chess;
 
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
+
 import java.util.Arrays;
 
 public final class Logic {
@@ -22,8 +23,10 @@ public final class Logic {
 
     private boolean free(Cell[] steps) throws OccupiedCellException {
         for (Cell step : steps) {
-            if (step != null) {
-                throw new OccupiedCellException("Cell is not free.");
+            for (Figure figure : figures) {
+                if (figure.position().equals(step)) {
+                    throw new OccupiedCellException("Cell is not free.");
+                }
             }
         }
         return true;
